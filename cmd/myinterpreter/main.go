a73527a8a3b34743d62dc8c021f986ccb9459008
Andrew Rodriguez <andrewrod003@gmail.com>
2024-12-12 12:43:44 -0800

    codecrafters submit [skip ci]

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -39,7 +39,12 @@ func main() {
 			fmt.Println("LEFT_PAREN ( null")
 		case ')':
 			fmt.Println("RIGHT_PAREN ) null")
+		case '{':
+			fmt.Println("LEFT_BRACE { null")
+		case '}':
+			fmt.Println("RIGHT_BRACE } null")
 		}
+
 	}
 
 	fmt.Println("EOF  null")
